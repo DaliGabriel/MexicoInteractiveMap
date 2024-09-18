ec6ea72a107929fee2d0ea7d144d2337d365a7cc
Gabriel Ahumada <gahumada@nice.com.mx>
2024-09-18 15:47:58 -0600

    map-center and dark mode with geojson on the same directory

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -4,6 +4,7 @@
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css";
 import "leaflet-defaulticon-compatibility";
+import { features } from "../app/data/mexicoHigh.json";
 
 import {
   MapContainer,
@@ -29,31 +30,39 @@ function GetCoordinates() {
 }
 
 export default function Map() {
-  const position: LatLngExpression = [20.63087146186356, -103.2211034886408]; // Explicitly type position
-  const [mexicoMunicipiosGeoJSON, setMexicoMunicipiosGeoJSON] = useState(null); // State for GeoJSON data
 
-  useEffect(() => {
-    // Fetch GeoJSON data from the provided URL
-    fetch(
-      "https://raw.githubusercontent.com/angelnmara/geojson/master/mexicoHigh.json"
-    )
-      .then((response) => response.json())
-      .then((data) => setMexicoMunicipiosGeoJSON(data))
-      .catch((error) => console.error("Error fetching GeoJSON:", error));
-  }, []); // Empty dependency array ensures fetching only once on component mount
+  //** Last option to put the name of the states
+  const position: LatLngExpression = [20.63087146186356, -103.2211034886408]; // Explicitly type position
+  const [mexicoMunicipiosGeoJSON, setMexicoMunicipiosGeoJSON] = useState(features);
+
+  // useEffect(() => {
+  //   // Fetch GeoJSON data from the provided URL
+  //   fetch(
+  //     "https://raw.githubusercontent.com/angelnmara/geojson/master/mexicoHigh.json"
+  //   )
+  //     .then((response) => response.json())
+  //     .then((data) => setMexicoMunicipiosGeoJSON(data))
+  //     .catch((error) => console.error("Error fetching GeoJSON:", error));
+  // }, []); // Empty dependency array ensures fetching only once on component mount
 
   return (
     <MapContainer
       center={position}
       zoom={4.5}
-      scrollWheelZoom={true}
-      style={{ height: "700px", width: "800px" }}
+      scrollWheelZoom={false}
+      style={{ height: "650px", width: "800px", borderRadius: "5px" }}
       attributionControl={false} // Hide the default attribution control
+      doubleClickZoom={false}
+      dragging={false}
+      zoomControl={false}
     >
       <GetCoordinates />
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        // url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        url="https://{s}.basemaps.cartocdn.com/rastertiles/dark_nolabels/{z}/{x}/{y}.png
+"
+
       />
       {mexicoMunicipiosGeoJSON && (
         <GeoJSON 
