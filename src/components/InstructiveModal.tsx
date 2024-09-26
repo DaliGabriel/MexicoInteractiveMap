eced1a2355dc774707a6389abee1bb3a882d411c
Gabriel Ahumada <gahumada@nice.com.mx>
2024-09-26 17:12:11 -0600

    modal user guide added

---
+++ src/components/InstructiveModal.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+interface InstructiveModalProps {
+  selectedState: string | null;
+}
+
+const InstructiveModal = ({ selectedState }: InstructiveModalProps) => {
+  const animationClass = selectedState ? "" : "animate__infinite";
+
+  return (
+    <div className="relative z-50 bg-white bg-opacity-90 h-40 w-60 mx-auto mt-20 rounded-lg shadow-2xl ">
+      <p className="text-center text-lg pt-4 font-bold">
+        Tap a state to explore more details. Scroll down to learn everything
+        about it!
+      </p>
+      <br />
+      <div
+        className={`text-center animate__animated animate__heartBeat animate__delay-2s ${animationClass}`}
+      >
+        👇
+      </div>
+    </div>
+  );
+};
+
+export default InstructiveModal;
