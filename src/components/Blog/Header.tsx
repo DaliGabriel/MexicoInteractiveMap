48f8e523695f0a76995e024c07fa91f42512786f
Gabriel Ahumada <gahumada@nice.com.mx>
2024-10-23 13:05:21 -0600

    header sticky to fixed

--- src/components/Blog/Header.tsx
+++ src/components/Blog/Header.tsx
@@ -3,7 +3,7 @@ import { FaMap, FaUser } from "react-icons/fa"; // Importing icons from FontAwes
 
 export default function Header() {
   return (
-    <header className="bg-white dark:bg-gray-800 w-full px-6 py-5 sticky top-0 shadow-md dark:shadow-gray-900">
+    <header className="bg-white dark:bg-gray-800 w-full px-6 py-5 fixed top-0 shadow-md dark:shadow-gray-900">
       <div className="flex justify-between items-center">
         {/* Left Icon */}
         <Link href={"/interactive-map"}>
